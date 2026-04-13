7cd8e772a3d1f5f7363e4bc845898112cba0f6c3
KRISH LAKHOTIA <lakhotiakrish07@gmail.com>
2026-04-13 09:20:03 +0530

    Delete PRACTICE 1 (BASICS OF CPP) directory

--- PRACTICE 1 (BASICS OF CPP)/floatmultipication.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std ;
-int main(){
-    float x , y ; 
-    cout<<"enter 2 number to multiply :"<<endl;
-    cin>>x>>y ;
-    cout<<"x X y ="<<x*y<<endl;
-    return 0 ;
-}
